df9c12aa9f3e799a5bc2d069f42ca9a56f63c59d
OzRagwort <ozragwort@gmail.com>
2020-12-08 21:39:50 +0900

    homePage 채널 명, 업로드 날짜 수정

---
+++ lib/function/time_function.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+
+import 'package:peton/model/VideoDuration.dart';
+
+String uploadTimeCheck(String publishedDate) {
+  var videoDateTime = DateTime.parse(publishedDate);
+  var utcDateTime = DateTime.now().subtract(DateTime.now().timeZoneOffset);
+  var durationTime = utcDateTime.difference(videoDateTime);
+  VideoDuration videoDuration = new VideoDuration(
+    durationTime.inDays ~/ 365,
+    (durationTime.inDays % 365) ~/ 30,
+    (durationTime.inDays % 365) - (durationTime.inDays % 365) ~/ 30,
+    durationTime.inHours - durationTime.inDays * Duration.hoursPerDay,
+    durationTime.inMinutes - durationTime.inHours * Duration.minutesPerHour,
+    durationTime.inSeconds - durationTime.inMinutes * Duration.secondsPerMinute
+  );
+
+  if(videoDuration.beforeYear == 0) {
+    if(videoDuration.beforeMonth == 0) {
+      if(videoDuration.beforeDay == 0) {
+        if(videoDuration.beforeHour == 0) {
+          if(videoDuration.beforeMinute == 0) {
+            return videoDuration.beforeSecond.toString() + ' 초 전';
+          }
+          return videoDuration.beforeMinute.toString() + ' 분 전';
+        }
+        return videoDuration.beforeHour.toString() + ' 시간 전';
+      }
+      return videoDuration.beforeDay.toString() + ' 일 전';
+    }
+    return videoDuration.beforeMonth.toString() + ' 달 전';
+  }
+  return videoDuration.beforeYear.toString() + ' 년 전';
+
+}
---
+++ lib/model/VideoDuration.dart
@@ -0,0 +1,12 @@
+class VideoDuration {
+
+  final int beforeYear;
+  final int beforeMonth;
+  final int beforeDay;
+  final int beforeHour;
+  final int beforeMinute;
+  final int beforeSecond;
+
+  VideoDuration(this.beforeYear, this.beforeMonth, this.beforeDay, this.beforeHour, this.beforeMinute, this.beforeSecond);
+
+}
