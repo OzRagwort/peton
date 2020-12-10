47121a8b1cf06a308ada9c9198392b11458f010b
OzRagwort <ozragwort@gmail.com>
2020-12-11 05:56:44 +0900

    왼쪽 여백 주는 위젯 추가

--- lib/widgets/Line.dart
+++ lib/widgets/Line.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 Widget get space => const SizedBox(height: 10);
 
+Widget get spaceLeft => const SizedBox(width: 10);
+
 Widget get divline => const Divider(color: Colors.grey,);
 
 Widget get verticalDivline => const VerticalDivider(color: Colors.grey,);
