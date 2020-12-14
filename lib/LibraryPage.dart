23a35d12e9e37a205872a9261aa4c9ca4823bd82
OzRagwort <ozragwort@gmail.com>
2020-12-14 17:11:51 +0900

    LibraryPage sqflite 내부 DB를 이용하여 저장하는 기능 추가

--- lib/LibraryPage.dart
+++ lib/LibraryPage.dart
@@ -1,13 +1,114 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:peton/Server.dart';
+import 'package:peton/database/LibraryVideosDb.dart';
+import 'package:peton/model/LibraryVideos.dart';
+import 'package:peton/widgets/Cards.dart';
 
-class LibraryPage extends StatelessWidget {
+import 'model/VideosResponse.dart';
 
+class LibraryPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  _LibraryPageState createState() => _LibraryPageState();
+}
 
-    return Center(child: Text("Hello library"));
+class _LibraryPageState extends State<LibraryPage> {
+
+  LibraryVideos libraryVideos;
+
+  void insertLib() {
+
+    var getData = server.getRandbyCategoryId('1', 1);
+    getData.then((value) => setState(() {
+      libraryVideos = LibraryVideos(
+        channelId : value[0].channels.channelId,
+        channelName : value[0].channels.channelName,
+        channelThumbnail : value[0].channels.channelThumbnail,
+        videoId : value[0].videoId,
+        videoName : value[0].videoName,
+        videoThumbnail : value[0].videoThumbnail,
+        videoPublishedDate : value[0].videoPublishedDate,
+        videoEmbeddable : value[0].videoEmbeddable,
+      );
+      LibraryVideosDb().insertLibraryVideo(libraryVideos);
+    }));
 
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<LibraryVideos>>(
+        future: LibraryVideosDb().getAllLibraryVideos(),
+        // builder: (BuildContext context, AsyncSnapshot<List<LibraryVideos>> snapshot) {
+        builder: (context, snapshot) {
+
+          if(snapshot.hasData) {
+
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+
+                LibraryVideos item = snapshot.data[index];
+
+                VideosResponse videosResponse = item.toVideosResponse();
+
+                return videoCardSmall(videosResponse, MediaQuery.of(context).size.width);
+
+                // return Dismissible(
+                //   key: UniqueKey(),
+                //   onDismissed: (direction) {
+                //     LibraryVideosDb().deleteLibraryVideo(item.videoId);
+                //     setState(() {});
+                //   },
+                //   child: Center(child: Text(item.videoName)),
+                // );
+              },
+            );
+          }
+          else
+          {
+            return Center(child: CircularProgressIndicator(),);
+          }
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.delete),
+            onPressed: () {
+              //모두 삭제 버튼
+              log('deleteAll');
+              LibraryVideosDb().deleteAllLibraryVideos();
+              setState(() {});
+            },
+          ),
+          SizedBox(height: 8.0),
+          FloatingActionButton(
+            child: Icon(Icons.refresh),
+            onPressed: () {
+              //새로고침
+              log('refresh');
+              setState(() {});
+            },
+          ),
+          SizedBox(height: 8.0),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              //추가 버튼
+              log('insert');
+              insertLib();
+              // setState(() {});
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
+
+
---
+++ lib/database/LibraryVideosDb.dart
@@ -0,0 +1,120 @@
+
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:peton/model/LibraryVideos.dart';
+import 'package:sqflite/sqflite.dart';
+
+final String TableName = 'LibraryVideos';
+
+class LibraryVideosDb {
+
+  LibraryVideosDb._();
+  static final LibraryVideosDb _db = LibraryVideosDb._();
+  factory LibraryVideosDb() => _db;
+
+  static Database _database;
+
+  Future<Database> get database async {
+    if(_database != null) return _database;
+
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'library_videos_db.db');
+
+    log('init');
+
+    return await openDatabase(
+        path,
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute('''
+          CREATE TABLE $TableName(
+            channelId TEXT, 
+            channelName TEXT, 
+            channelThumbnail TEXT, 
+            videoId TEXT, 
+            videoName TEXT, 
+            videoThumbnail TEXT, 
+            videoPublishedDate TEXT, 
+            videoEmbeddable INTEGER
+          )
+        ''');
+        },
+        onUpgrade: (db, oldVersion, newVersion){}
+    );
+  }
+
+  //Create insert
+  Future<void> insertLibraryVideo(LibraryVideos libraryVideos) async {
+    final db = await database;
+
+    await db.insert(
+      TableName,
+      libraryVideos.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  //Read one
+  Future<LibraryVideos> getLibraryVideo(String videoId) async {
+    final db = await database;
+
+    var res = await db.rawQuery('SELETE * FROM $TableName WHERE videoId = ?', [videoId]);
+    
+    return res.isNotEmpty ? LibraryVideos(
+      channelId : res.first['channelId'],
+      channelName : res.first['channelName'],
+      channelThumbnail : res.first['channelThumbnail'],
+      videoId : res.first['videoId'],
+      videoName : res.first['videoName'],
+      videoThumbnail : res.first['videoThumbnail'],
+      videoPublishedDate : res.first['videoPublishedDate'],
+      videoEmbeddable : res.first['videoEmbeddable'],
+    ) : Null;
+  }
+
+  //Read All
+  Future<List<LibraryVideos>> getAllLibraryVideos() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(TableName);
+
+    return List.generate(maps.length, (index) {
+      return LibraryVideos(
+        channelId : maps[index]['channelId'],
+        channelName : maps[index]['channelName'],
+        channelThumbnail : maps[index]['channelThumbnail'],
+        videoId : maps[index]['videoId'],
+        videoName : maps[index]['videoName'],
+        videoThumbnail : maps[index]['videoThumbnail'],
+        videoPublishedDate : maps[index]['videoPublishedDate'],
+        videoEmbeddable : maps[index]['videoEmbeddable']==1?true:false,
+      );
+    });
+  }
+
+  //Delete
+  Future<void> deleteLibraryVideo(String videoId) async {
+    final db = await database;
+
+    await db.delete(
+      TableName,
+      where: "videoId = ?",
+      whereArgs: [videoId],
+    );
+
+  }
+
+  //Delete All
+  Future<void> deleteAllLibraryVideos() async {
+    final db = await database;
+    db.rawDelete('DELETE FROM $TableName');
+  }
+
+}
