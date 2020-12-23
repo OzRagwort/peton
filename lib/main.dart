2ee7722d7a12f54de0278c52898d3d74c7f85310
OzRagwort <ozragwort@gmail.com>
2020-12-24 00:26:51 +0900

    Make the theme to enable various settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,37 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:peton/MainPage.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final AdaptiveThemeMode savedThemeMode;
+  const MyApp({Key key, this.savedThemeMode}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+    return AdaptiveTheme(
+      light: ThemeData(
+        brightness: Brightness.light,
         primarySwatch: Colors.blue,
+        accentColor: Colors.blueAccent,
       ),
-      home: MainPage(title: 'PetON', index: 0,),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        accentColor: Colors.blueAccent,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'PetON',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: MainPage(title: 'PetON', index: 0,),
+      ),
     );
   }
 }
