e5b1c5ebb348d095afb41031305993aab2258f78
OzRagwort <ozragwort@gmail.com>
2021-01-26 01:30:14 +0900

    Add initial commit KeywordsPage

---
+++ lib/KeywordsPage.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class KeywordsPage extends StatefulWidget {
+  @override
+  _KeywordsPageState createState() => _KeywordsPageState();
+}
+
+class _KeywordsPageState extends State<KeywordsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
