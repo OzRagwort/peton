8cfa0d09142e43bb36f081dfdd6395789ff1691e
OzRagwort <ozragwort@gmail.com>
2021-02-18 00:50:01 +0900

    Refactor ChannelInfoPage

--- lib/ChannelInfoPage.dart
+++ lib/ChannelInfoPage.dart
@@ -1,19 +1,18 @@
 
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:customizable_space_bar/customizable_space_bar.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:peton/MainPage.dart';
-import 'package:peton/enums/MyIcons.dart';
 import 'package:peton/model/Channels.dart';
 import 'package:peton/widgets/Cards.dart';
 import 'package:peton/widgets/CheckNetwork.dart';
 import 'package:peton/widgets/FavoriteIconBuilder.dart';
 import 'package:peton/widgets/Line.dart';
-import 'package:peton/widgets/TextForm.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'Server.dart';
 import 'VideoplayerPage.dart';
-import 'function/channelSubscriberCountCheck.dart';
 import 'model/VideosResponse.dart';
 
 class ChannelInfoPage extends StatefulWidget {
@@ -27,6 +26,8 @@ class ChannelInfoPage extends StatefulWidget {
 
 class _ChannelInfoPageState extends State<ChannelInfoPage> {
 
+  final dataKey = new GlobalKey();
+
   Channels channel;
   String sort = 'desc'; // 최신순 desc:최신, asc:오래된순, popular:인기순
   int count = 10;
@@ -39,24 +40,24 @@ class _ChannelInfoPageState extends State<ChannelInfoPage> {
   RefreshController(initialRefresh: false);
 
   Future<List<VideosResponse>> videosResponse;
-  List<VideosResponse> myList = new List<VideosResponse>();
+  List<VideosResponse> videosList = new List<VideosResponse>();
 
   void _onRefresh() async{
 
-    myList = new List<VideosResponse>();
+    videosList = new List<VideosResponse>();
     videosResponse = server.getByChannelIdSort(channel.channelId, sort, 1, count);
 
-    videosResponse.then((value) => setState(() {myList.addAll(value);}));
+    videosResponse.then((value) => setState(() {videosList = value;}));
 
     _refreshController.refreshCompleted();
   }
 
   void _onLoading() async{
 
-    int index = (myList.length ~/ 10) + 1;
+    int index = (videosList.length ~/ 10) + 1;
 
     videosResponse = server.getByChannelIdSort(channel.channelId, sort, index, count);
-    videosResponse.then((value) => myList.addAll(value));
+    videosResponse.then((value) => videosList.addAll(value));
 
     if(mounted)
       setState(() {});
@@ -73,12 +74,11 @@ class _ChannelInfoPageState extends State<ChannelInfoPage> {
       sort = 'asc';
     }
 
-    myList = new List<VideosResponse>();
+    videosList = new List<VideosResponse>();
     videosResponse = server.getByChannelIdSort(channel.channelId, sort, 1, count);
-    videosResponse.then((value) {
-      myList.addAll(value);
-      setState(() {});
-    });
+    videosResponse.then((value) {setState(() {
+      videosList = value;
+    });});
   }
 
   Widget _videosCartSmall(int listNum, double width) {
@@ -86,10 +86,10 @@ class _ChannelInfoPageState extends State<ChannelInfoPage> {
       onTap: () => {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => VideoPlayerPage(videoId: myList[listNum].videoId)),
+          MaterialPageRoute(builder: (context) => VideoPlayerPage(videoId: videosList[listNum].videoId)),
         )
       },
-      child: videoCard(myList[listNum], width),
+      child: videoCard(videosList[listNum], width),
     );
   }
 
@@ -105,187 +105,209 @@ class _ChannelInfoPageState extends State<ChannelInfoPage> {
     return items;
   }
 
+  Widget _sortDropDownMenu() {
+    return Container(
+      padding: const EdgeInsets.only(left: 10),
+      alignment: Alignment.centerLeft,
+      child: Row(
+        children: [
+          Icon(Icons.sort, size: 25,),
+          spaceLeft,
+          DropdownButton<String>(
+            icon: Icon(Icons.keyboard_arrow_down),
+            value: _sortingMethod,
+            iconSize: 24,
+            elevation: 16,
+            underline: Container(height: 0),
+            items: _dropDownMenuItems,
+            onChanged: (String newValue) {
+              _sortingMethod = newValue;
+              _sortRefresh(newValue);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+  
+  void _getVideos() {
+    videosResponse = server.getByChannelIdSort(channel.channelId, sort, 1, count);
+    videosResponse.then((value) {setState(() {
+      videosList = value;
+    });});
+  }
+
   @override
   void initState() {
     super.initState();
     channel = widget.channel;
     _dropDownMenuItems = getDropDownMenuItems();
     _sortingMethod = _dropDownMenuItems[1].value;
-    videosResponse = server.getByChannelIdSort(channel.channelId, sort, 1, count);
+    _getVideos();
   }
 
   @override
   Widget build(BuildContext context) {
-    int _selectedTabIndex = 0;
+
+    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(0),
-        child: AppBar(
-          title: Text(''),
-        ),
-      ),
-
-
-      body: CheckNetwork(
-        slidingUp: true,
-        body: Column(
-          children: [
-            /// 상단 채널 정보
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  channelThumbnailCircle(channel.channelThumbnail, MediaQuery.of(context).size.width / 10),
-                  verticalDivline,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        // Expanded(
-                        //   child: Text('title'),
-                        // ),
-                        textTitle(channel.channelName, 20.0),
-                        space,
-                        textTitle(ChannelSubscriberCountCheck(channel.subscribers), 15.0),
-                      ],
-                    ),
-                  ),
-                  FavoriteIconBuilder(channels: channel,),
-                ],
-              ),
+      body: SafeArea(
+        child: CheckNetwork(
+          slidingUp: true,
+          body: SmartRefresher(
+            enablePullDown: true,
+            enablePullUp: true,
+            header: MaterialClassicHeader(),
+            footer: CustomFooter(
+              loadStyle: LoadStyle.ShowWhenLoading,
+              builder: (BuildContext context, LoadStatus mode){
+                Widget body ;
+                /// 로드 완료 후
+                if(mode==LoadStatus.idle){
+                  body =  Text("pull up load");
+                }
+                /// ?
+                else if(mode==LoadStatus.loading){
+                  body =  CupertinoActivityIndicator();
+                }
+                /// ?
+                else if(mode == LoadStatus.failed){
+                  body = Text("Load Failed!Click retry!");
+                }
+                /// 로드하려고 풀업했을 때 나타는 것
+                else if(mode == LoadStatus.canLoading){
+                  body = Text("Load more");
+                }
+                /// ?
+                else{
+                  body = Text("No more Data");
+                }
+                return Container(
+                  height: 55.0,
+                  child: Center(child:body),
+                );
+              },
             ),
-
-            /// 정렬 박스
-            Container(
-              padding: const EdgeInsets.only(left: 10),
-              alignment: Alignment.centerLeft,
-              child: Row(
-                children: [
-                  Icon(Icons.sort, size: 25,),
-                  spaceLeft,
-                  DropdownButton<String>(
-                    icon: Icon(Icons.keyboard_arrow_down),
-                    value: _sortingMethod,
-                    iconSize: 24,
-                    elevation: 16,
-                    underline: Container(height: 0),
-                    items: _dropDownMenuItems,
-                    onChanged: (String newValue) {
-                      _sortingMethod = newValue;
-                      _sortRefresh(newValue);
+            controller: _refreshController,
+            onRefresh: _onRefresh,
+            onLoading: _onLoading,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  pinned: true,
+                  expandedHeight: (MediaQuery.of(context).size.width / 16) * 9,
+                  leading: IconButton(
+                    icon: Icon(Icons.keyboard_backspace),
+                    onPressed: () {
+                      Navigator.pop(context);
                     },
                   ),
-                ],
-              ),
-            ),
-
-            /// 리스트 뷰
-            Expanded(
-              child: SmartRefresher(
-                enablePullDown: true,
-                enablePullUp: true,
-                header: MaterialClassicHeader(),
-                footer: CustomFooter(
-                  loadStyle: LoadStyle.ShowWhenLoading,
-                  builder: (BuildContext context, LoadStatus mode){
-                    Widget body ;
-                    /// 로드 완료 후
-                    if(mode==LoadStatus.idle){
-                      body =  Text("pull up load");
-                    }
-                    /// ?
-                    else if(mode==LoadStatus.loading){
-                      body =  CupertinoActivityIndicator();
-                    }
-                    /// ?
-                    else if(mode == LoadStatus.failed){
-                      body = Text("Load Failed!Click retry!");
-                    }
-                    /// 로드하려고 풀업했을 때 나타는 것
-                    else if(mode == LoadStatus.canLoading){
-                      body = Text("Load more");
-                    }
-                    /// ?
-                    else{
-                      body = Text("No more Data");
-                    }
-                    return Container(
-                      height: 55.0,
-                      child: Center(child:body),
-                    );
-                  },
-                ),
-                controller: _refreshController,
-                onRefresh: _onRefresh,
-                onLoading: _onLoading,
-                child: ListView.builder(
-                    itemCount: myList?.length + 10,
-                    // ignore: missing_return
-                    itemBuilder: (context, index) {
-                      if (index == 0 && myList.length == 0) {
-                        return FutureBuilder<List<VideosResponse>>(
-                          future: videosResponse,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              myList.addAll(snapshot.data);
-                              return _videosCartSmall(index, MediaQuery.of(context).size.width);
-                            } else if (snapshot.hasError) {
-                              return Text("${snapshot.error}");
-                            }
-                            return Center(
-                              child: Image.asset(
-                                "lib/assets/spinner.gif",
-                                fit: BoxFit.fill,
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  flexibleSpace: CustomizableSpaceBar(
+                    builder: (context, scrollingRate) {
+                      String bannerImage = channel.bannerExternalUrl != null ? channel.bannerExternalUrl : channel.channelThumbnail;
+                      return Container(
+                        child: Stack(
+                          children: <Widget>[
+                            ExtendedImage.network(
+                              bannerImage,
+                              width: width,
+                              fit: BoxFit.fitWidth,
+                              cache: true,
+                              filterQuality: FilterQuality.high,
+                              loadStateChanged: (ExtendedImageState state) {
+                                switch (state.extendedImageLoadState) {
+                                  case LoadState.loading:
+                                    return SizedBox(
+                                      width: width,
+                                      child: Center(child: CupertinoActivityIndicator(),),
+                                    );
+                                    break;
+                                  case LoadState.completed:
+                                    return ExtendedRawImage(
+                                      image: state.extendedImageInfo?.image,
+                                      width: width,
+                                      fit: BoxFit.fitWidth,
+                                    );
+                                    break;
+                                  case LoadState.failed:
+                                    return SizedBox(
+                                      width: width,
+                                      child: Container(),
+                                    );
+                                    break;
+                                }
+                                return null;
+                              },
+                            ),
+                            Container(
+                              width: width,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Theme.of(context).scaffoldBackgroundColor.withOpacity(1.0 - (0.3 * scrollingRate)),
+                                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.7 * scrollingRate),
+                                  ],
+                                  begin: Alignment.bottomCenter,
+                                  end: FractionalOffset(0.5, 0.5 * (1 - scrollingRate)),
+                                ),
                               ),
-                            );
-                          },
-                        );
-                      }
-                      if (myList.length > index) {
-                        return _videosCartSmall(index, MediaQuery.of(context).size.width);
-                      }
-                    }
+                              // padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0 + 35 * scrollingRate),
+                              padding: EdgeInsets.only(bottom: 5.0, left: 15.0 + 35 * scrollingRate, right: 15),
+                              child: Align(
+                                alignment: FractionalOffset(0.0, 1 - (0.5 * scrollingRate)),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: AutoSizeText(
+                                        channel.channelName,
+                                        minFontSize: 20,
+                                        maxLines: scrollingRate > 0.6 ? 1 : null,
+                                        overflow: scrollingRate > 0.6 ? TextOverflow.ellipsis : null,
+                                        style: TextStyle(
+                                          color: Color(Theme.of(context).scaffoldBackgroundColor.value ^ 0xFFFFFF),
+                                          fontSize: 28 - 20 * scrollingRate,
+                                        ),
+                                      ),
+                                    ),
+                                    FavoriteIconBuilder(channels: channel,),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
+                SliverList(
+                  key: dataKey,
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                    if (index == 0) {
+                      return _sortDropDownMenu();
+                    } else {
+                      return _videosCartSmall(index - 1, MediaQuery.of(context).size.width);
+                    }
+                  },
+                    childCount: videosList.length + 1,
+                  ),
+                )
+              ],
             ),
-
-          ],
+          ),
         ),
       ),
-
-
-
-      bottomNavigationBar: SizedBox(
-        height: 50,
-        child: BottomNavigationBar(
-          iconSize: 21,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: _selectedTabIndex == 0 ? MyIcons.homePageIconFill : MyIcons.homePageIcon,
-              title: Text('Home', style: TextStyle(fontSize: 12),),
-            ),
-            BottomNavigationBarItem(
-              icon: _selectedTabIndex == 1 ? MyIcons.favoritePageIconFill : MyIcons.favoritePageIcon,
-              title: Text('Favorite', style: TextStyle(fontSize: 12),),
-            ),
-            BottomNavigationBarItem(
-              icon: _selectedTabIndex == 2 ? MyIcons.libraryPageIconFill : MyIcons.libraryPageIcon,
-              title: Text('Library', style: TextStyle(fontSize: 12),),
-            ),
-          ],
-          currentIndex: _selectedTabIndex,
-          onTap: (index) {
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => MainPage(title: 'PetON', index: index,),
-              ),
-                  (route) => false,
-            );
-          },
-        ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            heroTag: null,
+            child: Icon(Icons.keyboard_arrow_up),
+            onPressed: () => Scrollable.ensureVisible(dataKey.currentContext, duration: const Duration(seconds: 2), curve: Curves.fastOutSlowIn),
+          ),
+        ],
       ),
     );
   }
