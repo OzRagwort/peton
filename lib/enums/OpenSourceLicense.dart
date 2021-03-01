7bcf92fadf85586a2851d6ff139aa0037b4571fd
OzRagwort <ozragwort@gmail.com>
2021-03-01 20:48:47 +0900

    Add OpenSourceLicense admob_flutter

--- lib/enums/OpenSourceLicense.dart
+++ lib/enums/OpenSourceLicense.dart
@@ -2,6 +2,7 @@
 import 'package:peton/model/OpenSourceLicenseModel.dart';
 
 // Adaptive Theme
+// admob_flutter
 // Auto animated
 // AutoSizeText
 // carousel_slider
@@ -245,6 +246,30 @@ http://www.apache.org/licenses/
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.''',
+    ));
+
+    osModels.add(new OpenSourceLicenseModel(
+      name: "admob_flutter",
+      gitLink: "https://github.com/kmcgill88/admob_flutter",
+      copyright: '''Copyright © 2018, Youssef Kababe
+Copyright © 2019, Kevin McGill <kevin@mcgilldevtech.com>''',
+      license: "ISC License",
+      licenseDetails: '''ISC License
+
+Copyright (c) 2018, Youssef Kababe
+Copyright (c) 2019, Kevin McGill <kevin@mcgilldevtech.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.''',
     ));
 
     osModels.add(new OpenSourceLicenseModel(
